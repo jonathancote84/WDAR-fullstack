d7181f65e69979aadc3d91f6082ae2725cbd47e0
jonathancote84 <jonathancote92@gmail.com>
2020-10-07 13:53:11 -0500

    initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b8a1ce375c606422271a17b1b8cee2ff'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '95f76e05-16a3-4b92-b63c-3bcce2cba92b'
+};
