028a672f368f92bf042955ce48d2a25ed4b0a118
jonathancote84 <jonathancote92@gmail.com>
2020-12-02 14:07:51 -0600

    heroku test

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         filename: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'wdar_droplet'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'wdar_droplet'),
+        password: env('DATABASE_PASSWORD', 'bicpow'),
       },
       options: {
         ssl: false,
