9d870b42a45c7527d3eaca33be3a65b6e8dd160f
jonathancote84 <jonathancote92@gmail.com>
2020-12-07 14:36:35 -0600

    sqlite local

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,15 +4,11 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        filename: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'wdar_droplet'),
-        password: env('DATABASE_PASSWORD', 'bicpow'),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
-        ssl: false,
+        useNullAsDefault: true,
       },
     },
   },
